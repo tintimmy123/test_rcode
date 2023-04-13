bcf78384a6f3e3493be0dd0918a277644c69cc56
tintimmy123 <easytimwork@gmail.com>
2023-04-13 17:35:02 +1200

    new

--- example.R
+++ example.R
@@ -6,3 +6,12 @@ use_git_config(user.name="tintimmy123", user.email="easytimwork@gmail.com")
 head(car)
 
 2+2
+
+usethis::create_github_token()
+#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
+
+gitcreds::gitcreds_set("http://github.com")
+
+usethis::edit_git_ignore() #then add it in the gitignore doc
+
+usethis::edit_git_ignore(scope = "project")
